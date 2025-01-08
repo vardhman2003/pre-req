5d83a8d1bcdf39edfabfcafde4bae78a734c7028
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-01-08 12:06:05 +0530

    Day1 codes

---
+++ Twosum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Twosum{
+public static void main(String [] args){
+Scanner inp1=new Scanner(System.in);
+int input1=inp1.nextInt();
+Scanner inp2=new Scanner(System.in);
+int input2=inp2.nextInt();
+System.out.println(input1+input2);
+}
+}
